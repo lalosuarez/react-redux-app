4c6ee45295b87d0f608f5b5798d8b032a9303e7d
Eduardo Suarez <eduardo.suarez_vargas.ext@nokia.com>
2017-10-25 19:16:23 -0500

    Adding components

---
+++ src/Greeting/Greeting.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Greeting extends Component {
+  render() {
+    return (
+        <p className="App-intro">
+          Hello there!
+        </p>
+    );
+  }
+}
+
+export default Greeting;
