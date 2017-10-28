4a265b7f9b3dd52bfcfac0f9cbe4a60e669e97a6
Eduardo Suarez <eduardo.suarez_vargas.ext@nokia.com>
2017-10-28 15:14:16 -0500

    Adding new post component

---
+++ src/Posts/NewPost.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NewPost extends Component {
+  render() {
+    return (
+        <h2>Create new Post</h2>
+    );
+  }
+}
+
+export default NewPost;
--- src/Routes.js
+++ src/Routes.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './App';
 import Posts from './Posts/Posts';
+import NewPost from './Posts/NewPost';
 
 export default (
     <Route path="/" component={ App }>
         <IndexRoute component={ Posts } />
+        <Route path="posts" component={ Posts } />
+        <Route path="posts/new" component={ NewPost } />
     </Route>
 );
